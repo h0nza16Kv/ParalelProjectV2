4e72d11af5cb8450a4ff546e6fc157746f448487
h0nza16Kv <kvasnicka@spsejecna.cz>
2025-12-07 18:11:15 +0100

    .

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ STOP = object()
 LINES_PER_BLOCK = 100
 PATTERNS = [r'error', r'warning', r'failed', r'exception']
 INPUT_PATHS = ["input.txt", "another_log.txt"]
-OUTPUT_PATH = "src/results.json"
+OUTPUT_PATH = "results.json"
